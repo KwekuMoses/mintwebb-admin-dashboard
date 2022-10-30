76930264ce18d507e20b37989c9df8313cd20499
kweku moses <kweku.moses@hotmail.com>
2022-10-30 10:57:10 +0100

    Replace Switch with Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Redirect, Route } from 'react-router';
+import { Routes, Redirect, Route } from 'react-router';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { useState, createContext, useContext } from "react";
 import Login from './Login';
@@ -12,21 +12,21 @@ export const UserContext = createContext();
 // const UserContext = createContext();
 // const [user, setUser] = useState("Jesse Hall");
 
-function App () {
-    const [user,setUser] = useState("")
+function App() {
+    const [user, setUser] = useState("")
 
-  return(
+    return (
 
 
-    <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/register' component={Register}  />
-            <Route path='/dashboard' component={Dashboard}  user={user}/>
-            {/* <Route component={NotFound}/> */}
-        </Switch>
-    </BrowserRouter>
-);
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' component={Login} />
+                <Route exact path='/register' component={Register} />
+                <Route path='/dashboard' component={Dashboard} user={user} />
+                {/* <Route component={NotFound}/> */}
+            </Routes>
+        </BrowserRouter>
+    );
 
 }
 
